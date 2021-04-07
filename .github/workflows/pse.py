b6290a6839ab318cd6a3d1d4df420cbcfa9cd602
david-tcs <66868854+david-tcs@users.noreply.github.com>
2021-04-07 15:29:06 +0800

    commit

---
+++ .github/workflows/pse.py
@@ -0,0 +1,17 @@
+#PCA
+
+pca = PCA(n_components=2)
+pca_result = pca.fit_transform(df[feat_cols].values)
+df['pca-one'] = pca_result[:,0]
+df['pca-two'] = pca_result[:,1] 
+print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
+
+plt.figure(figsize=(16,10))
+sns.scatterplot(
+    x="pca-one", y="pca-two",
+    hue="y",
+    palette=sns.color_palette("hls", 10),
+    data=df.loc[rndperm,:],
+    legend="full",
+    alpha=0.3
+)
